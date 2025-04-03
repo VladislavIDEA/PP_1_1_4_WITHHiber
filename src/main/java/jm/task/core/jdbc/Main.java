5d0691c6eeef85bcf51dfb75d1d3ff05e9223d78
Vladislav Ilyasov <vladislav.aleksandrovic1@mail.ru>
2025-04-03 23:54:19 +0300

    fix sout

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,24 +11,14 @@ public class Main {
         UserServiceImpl userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Владислав", "Илясов", (byte) 25);
-        System.out.println("User с именем Владислав добавлен в базу данных");
-
         userService.saveUser("Петр", "Петров", (byte) 30);
-        System.out.println("User с именем Петр добавлен в базу данных");
-
         userService.saveUser("Мария", "Сидорова", (byte) 28);
-        System.out.println("User с именем Мария добавлен в базу данных");
-
         userService.saveUser("Анна", "Никифорова", (byte) 22);
-        System.out.println("User с именем Анна добавлен в базу данных");
-
         List<User> users = userService.getAllUsers();
         for (User user : users) {
             System.out.println(user);
         }
-
         userService.cleanUsersTable();
-
         userService.dropUsersTable();
     }
 }
